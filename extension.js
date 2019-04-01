f89c46f14f34b487a2cc177fce99d02db010eb00
Connor Holyday <connorholyday@gmail.com>
2019-04-01 17:26:26 +0100

    Finish up v1 of extension

--- extension.js
+++ extension.js
@@ -1,72 +1,72 @@
 const vscode = require("vscode");
-const fs = require("fs");
+const fs = require("fs-extra");
 const path = require("path");
+const cosmiconfig = require("cosmiconfig");
+const explorer = cosmiconfig("architect", { cache: false });
 
-function mkdirpath(dirPath) {
-  if (!fs.accessSync(dirPath, fs.constants.R_OK | fs.constants.W_OK)) {
-    try {
-      fs.mkdirSync(dirPath);
-    } catch (e) {
-      mkdirpath(path.dirname(dirPath));
-      mkdirpath(dirPath);
-    }
-  }
-}
+/**
+ * @param {String} folder the folder to be built to
+ * @param {Object} config config object containing the options and paths
+ */
+async function start(folder, config) {
+  const { name, paths } = config;
+  let value;
 
-async function architect(uri) {
-  const fsPath = (uri && uri.fsPath) || undefined;
-  let folder;
-  let hasInitialPath = false;
-
-  if (fsPath) {
-    folder = path.dirname(fsPath);
-    hasInitialPath = true;
-  } else {
-    folder = await vscode.window.showInputBox({
-      prompt: "Architect to",
-      value: vscode.workspace.workspaceFolders[0].uri.fsPath
+  if (name) {
+    value = await vscode.window.showInputBox({
+      prompt: "Please enter the desired name"
     });
   }
 
-  if (!folder) {
-    throw new Error("No folder");
+  paths.forEach(path => {
+    const destination = value ? path.replace(/ARCHITECT_NAME/g, value) : path;
+
+    fs.ensureFile(`${folder}/${destination}`);
+  });
+}
+
+/**
+ * @param {Object} uri VSCode URI object from context menu
+ */
+async function architect(uri) {
+  const localConfig = await explorer.search(vscode.workspace.rootPath);
+
+  if (localConfig === null || localConfig.isEmpty) {
+    throw new Error(
+      "Architect is missing a config, find out how: https://github.com/connorholyday/architect#config"
+    );
   }
 
-  if (!hasInitialPath) {
-    mkdirpath(folder);
-  }
+  try {
+    const stat = await fs.lstat(uri.fsPath);
 
-  fs.writeFile(
-    folder + "/testing-architect.js",
-    "",
-    {
-      flag: "wx"
-    },
-    err => {
-      if (err) throw err;
+    if (stat.isFile()) {
+      const { dir } = path.parse(uri.fsPath);
+      start(dir, localConfig.config);
+      return;
     }
-  );
 
-  vscode.window.showInformationMessage(folder);
+    if (stat.isDirectory()) {
+      start(uri.fsPath, localConfig.config);
+      return;
+    }
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
     "extension.architect",
     architect
   );
-
   context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
